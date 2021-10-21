a2887f96c2bbfb053ce57467872ea98eb0505d02
shamim <maralmo137979@gmail.com>
2021-10-21 15:03:00 +0330

    add admin stuff

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from blog.models import *
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "avatar", "Description"]
+
+admin.site.register(UserProfile , UserProfileAdmin)
+
+
+
+class CategoryAmin(admin.ModelAdmin):
+    list_display = ["title", "cover"]
+
+
+admin.site.register(Category , CategoryAmin)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "created_at"]
+    search_fields = ["title" , "content"]
+
+admin.site.register(Article , ArticleAdmin)
+
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import User
 from django.db import models
+from ckeditor.fields import RichTextField
+from datetime import datetime
 
-# Create your models here.
+def Validate_File_Extention(value) :
+    import os 
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]
+    vali_extention = [".jpg" ,".png"]
+    if not ext.lower() in vali_extention :
+        raise ValidationError("unsupported file extention .")
+
+
+
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.FileField(upload_to="files/user_avatar" , null=True , blank= True , validators=[Validate_File_Extention] )
+    Description = models.CharField(max_length= 512 , null=False , blank= False)
+
+
+class Category (models.Model):
+    title = models.CharField(max_length=128 , null= False , blank= False)
+    cover = models.FileField(upload_to="files/category_cover" , validators= [Validate_File_Extention])
+
+
+class Article(models.Model):
+    title = models.CharField(max_length= 120 , null= False , blank= False)
+    cover = models.FileField(upload_to="files/article_cover/" , null= False , blank=False ,validators= [Validate_File_Extention])
+    content =RichTextField()
+    created_at = models.DateTimeField(default=datetime.now , blank= False)
+    category = models.ForeignKey(Category , on_delete= models.CASCADE)
+    author = models.ForeignKey(UserProfile , on_delete= models.CASCADE)
+
+
